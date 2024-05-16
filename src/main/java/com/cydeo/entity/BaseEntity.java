a31cc397008f58a51357a7f7e60f96ffa2502dfe
Human <vladislav.rogovoy@gmail.com>
2024-05-16 06:40:27 -0400

    persist and update functions added to automaticaly capture insert/update date/user

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -18,11 +15,30 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
     private Long insertUserId;
     private LocalDateTime lastUpdateDateTime;
     private Long lastUpdateUserId;
 
     private Boolean isDeleted=false;
+
+    // run below logic when record inserted.
+    @PrePersist
+    public void onPrePersist(){
+        this.insertDateTime=LocalDateTime.now();
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.insertUserId=1L;
+        this.lastUpdateUserId=1L;
+    }
+
+    // run below code when record updated
+    @PreUpdate
+    public void onPreUpdate(){
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.lastUpdateUserId=1L;
+    }
+
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,28 @@
-INSERT INTO roles(description, is_deleted) VALUES ('Admin', false);
-INSERT INTO roles(description, is_deleted) VALUES ('Manager', false);
-INSERT INTO roles(description, is_deleted) VALUES ('Employee', false);
+INSERT INTO roles(description, is_deleted, insert_date_time, insert_user_id) VALUES ('Admin', false, '2024-05-01 00:00:00.000000', 1);
+INSERT INTO roles(description, is_deleted, insert_date_time, insert_user_id) VALUES ('Manager', false, '2024-05-01 00:00:00.000000', 1);
+INSERT INTO roles(description, is_deleted, insert_date_time, insert_user_id) VALUES ('Employee', false, '2024-05-01 00:00:00.000000', 1);
 
-INSERT INTO users(first_name, last_name, user_name, pass_word, enabled, phone, role_id, gender, is_deleted)
-VALUES ('John', 'Smith', 'jsmith', 'arr', true, '212-593-0233', 1, 'MALE', false),
-       ('Suzan', 'Smith', 'ssmith', 'orr', true, '212-593-0234', 2, 'FEMALE', false),
-       ('Jonny', 'Smith', 'jsmithjr', 'irr', true, '212-593-0235', 3, 'MALE', false),
-       ('Suzan2', 'Smith', 's2smith', 'orr', true, '212-593-0234', 2, 'FEMALE', false),
-       ('Jonny2', 'Smith', 'j2smithjr', 'irr', true, '212-593-0235', 3, 'MALE', false);
+INSERT INTO users(first_name, last_name, user_name, pass_word, enabled, phone, role_id, gender, is_deleted, insert_date_time, insert_user_id)
+VALUES ('John', 'Smith', 'jsmith@cydeo.com', 'arr', true, '2125930233', 1, 'MALE', false, '2024-05-01 00:00:00.000000', 1),
+       ('Suzan', 'Smith', 'ssmith@cydeo.com', 'orr', true, '2125930234', 2, 'FEMALE', false, '2024-05-01 00:00:00.000000', 1),
+       ('Jonny', 'Smith', 'jsmithjr@cydeo.com', 'irr', true, '2125930235', 3, 'MALE', false, '2024-05-01 00:00:00.000000', 1),
+       ('Suzan2', 'Smith', 's2smith@cydeo.com', 'orr', true, '2125930234', 2, 'FEMALE', false, '2024-05-01 00:00:00.000000', 1),
+       ('Jonny2', 'Smith', 'j2smithjr@cydeo.com', 'irr', true, '2125930235', 3, 'MALE', false, '2024-05-01 00:00:00.000000', 1);
 
-insert into projects(project_name, project_code, project_detail, project_status, start_date, end_date, assigned_manager_id, complete_task_counts, unfinished_task_counts, is_deleted)
-select 'Project One', 'P01', 'First Project', 'OPEN', '2004-01-06', '2024-05-12', id, 0, 0, false from users where first_name='Suzan';
+insert into projects(project_name, project_code, project_detail, project_status,
+                     start_date, end_date, assigned_manager_id, complete_task_counts, unfinished_task_counts, is_deleted,
+                     insert_date_time, insert_user_id)
+select 'Project One', 'P01', 'First Project', 'OPEN',
+                    '2004-01-06', '2024-05-12', id, 0, 0, false,
+                    '2024-05-01 00:00:00.000000', 1 from users where first_name='Suzan';
 
-insert into projects(project_name, project_code, project_detail, project_status, start_date, end_date, assigned_manager_id, complete_task_counts, unfinished_task_counts, is_deleted)
-select 'Project Two', 'P02', 'Second Project', 'OPEN', '2007-05-08', '2024-05-12', id, 0, 0, false from users where first_name='Suzan';
+insert into projects(project_name, project_code, project_detail, project_status, start_date, end_date, assigned_manager_id, complete_task_counts, unfinished_task_counts, is_deleted,
+                     insert_date_time, insert_user_id)
+select 'Project Two', 'P02', 'Second Project', 'OPEN', '2007-05-08', '2024-05-12', id, 0, 0, false,
+                    '2024-05-01 00:00:00.000000', 1 from users where first_name='Suzan';
 
 
-insert into tasks (assigned_date, task_detail, task_status, task_subject, tid, assigned_employee_id, project_id, is_deleted)
-values (now(), 'Task One Detail', 'OPEN', 'Task One', 1, 3, 2, false),
-       (now(), 'Task Two Detail', 'OPEN', 'Task Two', 2, 3, 2, false);
+insert into tasks (assigned_date, task_detail, task_status, task_subject, tid, assigned_employee_id, project_id, is_deleted,
+                   insert_date_time, insert_user_id)
+values (now(), 'Task One Detail', 'OPEN', 'Task One', 1, 3, 2, false, '2024-05-01 00:00:00.000000', 1),
+       (now(), 'Task Two Detail', 'OPEN', 'Task Two', 2, 3, 2, false, '2024-05-01 00:00:00.000000', 1);
